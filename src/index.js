54f2438dafd4d8fcd1c2507cd9ea26d346ad007a
MosheDaniel <moo15daa@gmail.com>
2022-11-30 21:15:02 +0200

    on click examples

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './application/01_loops/App';
+import App from './application/02_onClick/App';
 import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
